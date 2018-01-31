0943cdd7575f91405317f122579bb142d09aaceb
alemk <alemseged.hailu@gmail.com>
2018-01-31 17:45:31 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+public class MainController {
+    @Autowired
+    RedditRepository redditRepository;
+    @RequestMapping("/")
+    public String listreddit(Model model){
+        model.addAttribute("reddits", redditRepository.findAll());
+        return "home";
+    }
+    @GetMapping("/add")
+    public String redditpost(Model model){
+        model.addAttribute("reddits", new Reddit());
+        return "addpost";
+    }
+    @PostMapping("/process")
+    public String processpost(@Valid Reddit reddit, BindingResult result)
+    {
+        if (result.hasErrors()){
+            return "home";
+        }
+        redditRepository.save(reddit);
+        return "redirect:/";
+    }
+    @RequestMapping("detail/{id}")
+    public String showreddit(@PathVariable("id") long id, Model model){
+        model.addAttribute("reddit", redditRepository.findOne(id));
+        return "showreddit";
+    }
+}
---
+++ src/main/java/com/example/demo/Reddit.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Reddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @NotNull
+    public String username;
+    @NotNull
+    public String title;
+    @NotNull
+    public String subject;
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
